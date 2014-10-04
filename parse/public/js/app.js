81b94c2b3b486fa0735ded90ad39b6f9974222fc
MaxFangX <maxfangx@gmail.com>
2014-10-04 14:32:08 -0700

    Base jQuery

--- parse/public/js/app.js
+++ parse/public/js/app.js
@@ -1,8 +1,28 @@
+//This initialize statement identifies the app
 Parse.initialize("0DjH3hlL03Nf8neV0qBuG8LfgzrGx6xZBOSN8zwi", 
 	"vD6Ar9262yegpOE1hXQm8XKMeNNMkpGd6DwmNYoU");
-var TestObject = Parse.Object.extend("TestObject");
+
+
+$("#brand").click(function(){
+	alert("sup faggot");
+})
+
+
+
+
+
+
+
+
+//To test that app.js works
+/*var main = function(){
+	alert('Page is ready!');
+}
+$(document).ready(main);*/
+
+/*var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
 testObject.save({foo: "bar"}).then(function(object) {
   var msgbox = Windows.UI.Popups.MessageDialog("yay! it worked");
   return msgbox.showAsync();
-});
+});*/
